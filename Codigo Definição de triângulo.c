d40f36ac53ece609e3a87f2cfc6b43976ffebedd
Luiz-Guerra-software <luizpaulotoniniguerrawork@gmail.com>
2026-05-31 20:08:32 -0300

    Update Codigo Definição de triângulo.c

--- Codigo Definição de triângulo.c
+++ Codigo Definição de triângulo.c
@@ -1,42 +1,22 @@
 #include<stdio.h>
 int main(){
-    int mat1[3][3],mat2[3][3],mult[3][3],j,i;
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf("\nDigite o valor da Matriz 1[%d][%d]:",i,j);
-            scanf("%d",mat1[i][j]);
-        }
+   float lado1,lado2,lado3;
+   printf("\ndigite o valor do primeiro lado:");
+   scanf("%f",&lado1);
+   printf("\ndigite o valor do segundo lado:");
+   scanf("%f",&lado2);
+   printf("\ndigite o valor do terceiro lado:");
+   scanf("%f",&lado3);
+   if(lado1<lado2+lado3 && lado2<lado1+lado3 && lado3<lado1+lado2){
+    printf("\nE um triangulo");
+    if(lado1==lado2 && lado2==lado3){
+        printf("\nE um triangulo equilatero");
+    }else if(lado1==lado2 && lado2!=lado3 || lado2==lado3 && lado3!=lado1 || lado1==lado3 && lado3!=lado2){
+        printf("\nE um triangulo isoceles");
+    }else if(lado1!=lado2 && lado2!=lado3 && lado1!=lado3){
+        printf("\nE um triangulo escaleno");
     }
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf("\nDigite o valor da Matriz 2[%d][%d]:",i,j);
-            scanf("%d",mat2[i][j]);
-        }
-    }
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            mult[i][j]=mat1[i][j]*mat2[i][j];
-        }
-    }
-    printf("\nA matriz 1 ficou:");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf(" %d ",mat1[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\nA matriz 2 ficou:");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf(" %d ",mat2[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\nA matriz multiplicada ficou:");
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            printf(" %d ",mult[i][j]);
-        }
-        printf("\n");
-    }
+   }else{
+       printf("\nNao e um triangulo!");
+    }return 0;
 }
